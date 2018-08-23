a2a796bcb888bb05dad53517ded64fbccb1b66c1
R-PC\R <sk118420@gmail.com>
2018-08-23 01:06:27 -0700

    Proc file and requirments are added

--- hello.py
+++ hello.py
@@ -3,12 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route("/comment")
+def comment():
     return "Hello World!"
 
 
-@app.route("/status")
-def status():
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+@app.route("/devices")
+def devices():
     return "Status of the devices"
- 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
